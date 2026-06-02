7896c6025bce36f659a82709838b82e87b2f99f6
Alberto Kleyber <albertok_alves@hotmail.com>
2026-06-02 11:59:25 -0300

    Update and rename Exemplo01.java to Produto.java

---
+++ Produto.java
@@ -0,0 +1,8 @@
+public class Produto {
+    String nome;
+    double preco;
+
+    public void exibirInformacoes() {
+        System.out.println("Produto: " + nome + " | Preço: R$" + preco);
+    }
+}
