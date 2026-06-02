1762294f7797f53cad266dac5d07c146dae7d34b
Alberto Kleyber <albertok_alves@hotmail.com>
2026-06-02 12:00:02 -0300

    Add main application class with product display

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto();
+
+        p1.nome = "Notebook";
+        p1.preco = 3500.00;
+
+        p1.exibirInformacoes();
+    }
+}
